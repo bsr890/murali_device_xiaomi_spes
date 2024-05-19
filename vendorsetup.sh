9d1b161434d4218571892235725895a0e8ba6161
bsr890 <bsonarati@gmail.com>
2024-05-19 05:51:09 +0000

    clone sources

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,10 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+# Vendor & Kernel Sources
+git clone https://github.com/crdroidandroid/android_vendor_xiaomi_spes.git -b 14.0 vendor/xiaomi/spes
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+
+# Lineage-21 Hardware Source
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
